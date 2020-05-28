b787cc3ba850d4b11817d429f2398ebb4964f809
AlanAlv <alan.alv96@gmail.com>
2020-05-28 02:30:43 -0500

    Form Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
-import Header from './components/Header'
+import Header from './components/Header';
+import styled from '@emotion/styled';
+import Form from './components/Form';
+
+const Container = styled.div`
+  max-width: 600px;
+  margin: 0 auto;
+`;
+
+const FormContainer = styled.div`
+  background-color: #FFF;
+  padding: 3rem;
+`;
 
 function App() {
   return (
-    <Header 
-      title='Car Insurance Quote'
-    />
+    <Container>
+      <Header 
+        title='Car Insurance Quote'
+      />
+
+      <FormContainer>
+        <Form 
+        />
+      </FormContainer>
+    </Container>
+
+  
   );
 }
 
