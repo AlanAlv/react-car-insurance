0c4fe4ddcc83ec9a8420ba964b070de2f4bb6076
AlanAlv <alan.alv96@gmail.com>
2020-05-28 01:52:52 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from './components/Header'
+
+function App() {
+  return (
+    <Header 
+      title='Car Insurance Quote'
+    />
+  );
+}
+
+export default App;
