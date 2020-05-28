3fbfccf7b63e700994d2a32a39b14c64a76cc461
AlanAlv <alan.alv96@gmail.com>
2020-05-28 02:05:19 -0500

    Header styled components

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,26 @@
-import React from 'react'
+import React from 'react';
+import styled from '@emotion/styled';
 
+const HeaderContainer = styled.header`
+    background-color: #26c6DA;
+    padding: 10px;
+    font-weight: bold;
+    color: #FFFFFF;
+`;
+
+const HeaderText = styled.h1`
+    font-size: 2rem;
+    margin: 0;
+    font-family: 'Slabo 27px', serif;
+    text-align: center;
+`
 const Header = ({title}) => {
     return ( 
-        <header>
-            <h1>
+        <HeaderContainer>
+            <HeaderText>
                 {title}
-            </h1>
-        </header>
+            </HeaderText>
+        </HeaderContainer>
      );
 }
  
