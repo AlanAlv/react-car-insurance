98545932df98606ea1930f57c4c7324473db795e
AlanAlv <alan.alv96@gmail.com>
2020-05-29 02:21:55 -0500

    Form styled components

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,64 @@
-import React from 'react'
+import React from 'react';
+import styled from '@emotion/styled';
+
+const Field = styled.div`
+    display: flex;
+    margin-bottom: 1rem;
+    align-items: center;
+
+`;
+
+const Label = styled.label`
+    flex: 0 0 100px;
+`;
+
+const Select = styled.select`
+    display: block;
+    width: 100%;
+    padding: 1rem;
+    border: 1px solid #e1e1e1;
+    -webkit-appearance: none;
+`;
+
+const InputRadio = styled.input`
+    margin: 0 1rem;
+`;
+
+const Button = styled.button`
+    background-color: #00838F;
+    font-size: 16px;
+    width: 100%;
+    padding: 1rem;
+    color: #fff;
+    text-transform: uppercase;
+    font-weight: bold;
+    border: none;
+    transition: background-color .3s ease;
+    margin-top: 2rem;
+
+    &:hover {
+        background-color: #26C6DA;
+        cursor: pointer;
+    }
+`;
 
 const Form = () => {
     return (  
         <form>
-            <div>
-                <label>Brand</label>
-                <select>
+            <Field>
+                <Label>Brand</Label>
+                <Select>
                     <option value="">-- Select --</option>
                     <option value="american">-- American --</option>
                     <option value="european">-- European --</option>
                     <option value="asian">-- Asian --</option>
 
-                </select>
-            </div>
+                </Select>
+            </Field>
 
-            <div>
-                <label>Year</label>
-                <select>
+            <Field>
+                <Label>Year</Label>
+                <Select>
                     <option value="">-- Seleccione --</option>
                     <option value="2021">2021</option>
                     <option value="2020">2020</option>
@@ -28,30 +70,25 @@ const Form = () => {
                     <option value="2014">2014</option>
                     <option value="2013">2013</option>
                     <option value="2012">2012</option>
-                </select>
-            </div>
+                </Select>
+            </Field>
 
-            <div>
-                <label>Plan</label>
-                <input 
+            <Field>
+                <Label>Plan</Label>
+                <InputRadio 
                     type="radio" 
                     name="plan" 
                     value="basic"
                 /> Basic
-            </div>
-
-            <div>
-                <label>Plan</label>
-                <input 
+                <InputRadio 
                     type="radio" 
                     name="plan" 
                     value="complete"
                 /> Complete
-            </div>
-
-            <button type="button">
+            </Field>
+            <Button type="button">
                 Quote
-            </button>
+            </Button>
         </form>
     );
 }
